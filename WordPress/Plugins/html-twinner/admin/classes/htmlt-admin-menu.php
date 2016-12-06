77de7da7e0e1f680d922d1d56cb6bc77c2e96a9c
Make-Websites <we@make-websites.co.uk>
2016-12-06 09:46:04 +0000

    Changes in htmlt_admin_menu class. Uploading and validating path

--- WordPress/Plugins/html-twinner/admin/classes/htmlt-admin-menu.php
+++ WordPress/Plugins/html-twinner/admin/classes/htmlt-admin-menu.php
@@ -9,22 +9,16 @@ class htmlt_admin_menu {
 	add_action('admin_init', array($this, 'htmlt_admin_init'));
 	}
 	
-	/* public function setHtmltd($htmltd) {
-	$this->_htmltd = $htmltd; }
-	
-	public function getHtmltd() {
-	return $this->_htmltd; } */
-	
 	public function htmlt_menu() { // Adding the options
-	add_options_page('<div class="boton">HTML Twinner Options</div>','HTML twinner','manage_options','html-twinner', array ($this, 'htmlt_settings_page'));
+	add_options_page('HTML Twinner Settings','HTML twinner','manage_options','html-twinner', array ($this, 'htmlt_settings_page'));
 	}
 	
 	public function htmlt_settings_page() {
 		?>
 	<div class="wrap">
-		<h2>HTML Twinner Options</h2>
+		<h2>HTML Twinner Settings</h2>
 		<div class="settings-container">
-		<form action="options.php" method="post">
+		<form action="options.php" method="POST">
 			<?php settings_fields('htmlt_options'); ?>
 			<?php do_settings_sections('html-twinner'); ?>
 			<input class="button-primary" name="Submit" type="submit" value="Save Changes" />
@@ -39,12 +33,12 @@ class htmlt_admin_menu {
 	public function htmlt_admin_init() {
 		register_setting(
 			'htmlt_options',
-			'htmlt_d',
-			'htmltd_valid'
+			'htmltd',
+			array($this, 'htmltd_validation')
 		);
 		add_settings_section(
 			'htmlt_admin_main',
-			'HTML-Twinner Settings',
+			'HTML-Twinner Working Directory',
 			array ($this, 'htmlt_section_text'),
 			'html-twinner'
 		);
@@ -59,36 +53,29 @@ class htmlt_admin_menu {
 	
 	// Draw the section header
 	public function htmlt_section_text() {
-		echo '<p>Path directory to create HTML file with post content or to retrieve post content from</p>';
+		echo '<p>Path directory to create HTML file with post content or to retrieve post content from</br>';
+		echo 'Paste or write the path in the box</p>'; 
 		}
 
 	// Display and fill the htmltd form field
 	public function htmltd_input() {
 		// get path option from the database
-		$hmtlt_optn = get_option('html_twinner_path'); //retrieves the path to the working directory from plugin option (if exists)
-		if ( isset( $_POST['file_d'] )) {
-					$provd = $_POST['file_d'];
-					echo 'Provd es '.$provd;
-					if (wp_verify_nonce( $_POST['htmltd'], 'htmltdc' ) && is_dir($provd));
-						$htmlt_d = (validate_file($provd))? $provd : NULL; ?>
-						<div class="notice notice-success is-dismissible"> 
-						<p><strong>Settings saved.</strong></p> 
-						</div> 
-			<?php } else {
-				//echo 'No hay post recibido'; ?>
-		<!--<input id='htmltd' type='file' name='htmld'/>-->
-		<form method="post">
-			<input type="text" name="file_d" size="110" />
-			<input class="primary-button" type="submit" name="select" value="Select Folder" />
-			<?php //wp_nonce_field( 'htmltdc', 'htmltd' ); ?>
-			</form>
-			
-<?php		}
+		$optn = esc_attr(get_option('htmltd')); // get options array of the plugin
+		$htmltd = (empty ($optn))? "Value not set" : $optn ; //retrieves the path to the working directory from plugin option (if exists)
+		echo "<input id='' name='htmltd' type='text' value='$htmltd' size='110'/>";
+		}
 		
 	//Validating 
-	function htmltd_valid($htmltd) {
-		return (validate_file( $htmltd) == 0);
-		}
-	}
-
+	function htmltd_validation($input) {
+		$valid = (file_exists($input) && !(is_file($input)))? $input : null;
+		if (empty($valid)) { //Not existing directory
+			$valid = null;
+			add_settings_error(
+			'htmlt_htmltd',
+			'htmltd_error',
+			'Directory does not exist!',
+			'error'
+			);	}	 
+		return $valid; 
+		} 
 }
