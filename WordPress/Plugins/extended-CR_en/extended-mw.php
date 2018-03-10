8b2bdfa64d2e1e87f1385b45abc3319bd6d765b1
Make-Websites <we@make-websites.co.uk>
2018-03-10 09:51:44 +0000

    Including ajax processing prices

---
+++ WordPress/Plugins/extended-CR_en/extended-mw.php
@@ -0,0 +1,168 @@
+<?php
+/*
+Plugin Name: Extended-MW
+Description: Extensions to MW Genesis sites
+Version: 1.0
+Author: Make-Websites 
+License: GPLv2
+*/
+
+//Registering bootstrap
+function mw_registers () {
+	
+$price = 450;
+$params = array(
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
+		'price' => $price);
+	wp_deregister_script('jquery'); //Deregister custom WordPress Jquery
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'); // Registering Google lib
+	wp_enqueue_style('bootstrap-style', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'); // Registering Bootstrap 4
+    wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
+	wp_enqueue_script( 'mw-js', get_stylesheet_directory_uri().'/mw.js');
+	wp_enqueue_script( 'mw-ajax', plugin_dir_url( __FILE__ ).'prices/ajax-prices.js');
+	wp_localize_script( 'mw-ajax', 'mw', $params);
+	wp_enqueue_style( 'mw-css', get_stylesheet_directory_uri().'/mw.css');
+	/*wp_enqueue_script('bootstrap_validator_js', '//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js');
+	wp_enqueue_style('bootstrap_validator_css', '//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/css/bootstrapValidator.min.css');*/
+}
+add_action('wp_enqueue_scripts', 'mw_registers');
+
+//Opening and closing Bootstrap container
+add_action ('genesis_before', 'mw_open_container');
+function mw_open_container() {
+	echo '<div class="container">';
+}
+add_action ('genesis_after', 'mw_close_container');
+function mw_close_container() {
+	echo '</div>';
+}
+
+//Remove header
+add_action('get_header', 'mw_remove_header');
+function mw_remove_header() {
+remove_action( 'genesis_header', 'genesis_do_header' );
+}
+//Custom header
+add_action ('genesis_header', 'mw_custom_header');
+function mw_custom_header() { ?>
+<div class="pt-1">
+<a href="<?php site_url() ?>/contact/" title="Contact Make-Websites team">
+	<img  class="img-fluid float-left align-middle" src="<?php site_url() ?>/images/make-websites.png" 
+		alt="Make Websites: Customizing WordPress" width="75px"></a>
+	<!--<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+	<h2><em><button type="button" style="margin-left:3px;" class="btn btn-success pull-left">We Make Websites</em></h2>
+	<a href="mailto:we@make-websites.co.uk" title="Write us">
+	<img  class="img-fluid float-left hidden-sm-down" src="<?php site_url() ?>/images/email-mw.gif" 
+		alt="Make Websites: Customizing WordPress" width="400px"></a></div>-->
+	<a href="http://es.make-websites.co.uk/"> <img class="img-fluid float-right align-top" src="http://make-websites.co.uk/images/es.png" 
+		alt="Make-Websites en español" title="Make-Websites en español"></a>
+</div>
+	 <?php
+}
+
+//* Customize the Genesis credits and footer
+add_filter( 'genesis_footer_creds_text', 'mw_footer_creds_text' );
+function mw_footer_creds_text() { ?>
+	<div class="row">
+	<div class="col-7">
+	<div class="creds"><p>
+	<?php echo 'Copyright &copy; ';
+	echo date('Y');
+	echo ' &middot'; ?> <a href="http://make-websites.co.uk" title="Customizing WordPress Websites">Make Websites</a>
+	</p></div></div>
+	<div class="col-5">
+	<a href="mailto:we@make-websites.co.uk" title="Write us"><img class="img-fluid float-right" src="<?php site_url() ?>/images/email-mw.gif" 
+		alt="Make Websites: Write us" width="300px"></a></div></div> <?php
+}
+
+
+function mw_login_logo() { // Customize login logo ?> 
+    <style type="text/css">
+        body.login div#login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo-mw_fig.png);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'mw_login_logo' );
+
+function mw_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'mw_login_logo_url' );
+
+function mw_login_logo_url_title() {
+    return 'Make Websites';
+}
+add_filter( 'login_headertitle', 'mw_login_logo_url_title' );
+
+// Processing the prices forms
+/*add_action('wp_ajax_mw_prices', 'mw_prices_form');
+add_action('wp_ajax_nopriv_mw_prices', 'mw_prices_form');
+function mw_prices_form() {
+include_once plugin_dir_url( __FILE__ ).'prices/price-add.php';
+
+$npages = $_POST['npages'];
+$pcase = $_POST['pcase'];
+
+$data = array();
+
+function validate_npages ($npages) {
+	
+		if (is_numeric($npages) and (intval($npages) == $npages)) {
+			if (($npages > 2) and ($npages < 11))
+				$er_message = 'correct';
+			else
+			$er_message = 'Numbers of pages must be higher than 2 and lower than 11';	}
+		else
+		$er_message = 'Please enter an integer number';
+return $er_message; }
+
+$message = validate_npages($npages);
+
+//if ($message == 'correct') {
+	
+	$np = new price_add($npages, $pcase);
+	$dat['price'] = $np->get_price();
+	//$page = $np->get_page();
+	
+	//$page = 'http://mw-en/prices/'.$page;
+	
+	/*echo '<p> La pagina es '.$page.'</p>';
+	echo '<p> y el precio es '.$_SESSION['nprice'].'</p>';
+	
+
+	//}
+
+$dat['message'] = $message;
+	
+$data = json_encode($data);
+die(); // required. To let AJAX know to end the request.*/
+
+add_action( 'wp_ajax_mw_prices_form', 'mw_prices_form' );
+add_action( 'wp_ajax_nopriv_mw_prices_form', 'mw_prices_form' );
+function mw_prices_form() {
+	$response = array();
+	$response['error'] = false;
+    
+		if (!is_numeric($_POST['npages']) or intval($_POST['npages']) != $_POST['npages']) {
+		$response['error'] = true;
+    	$response['error_message'] = 'Number of pages must be an integer number'; }
+ 	
+		elseif ($_POST['npages'] > 10 or $_POST['npages'] < 3) {
+		$response['error'] = true;
+    	$response['error_message'] = 'Number of pages must be between 3 and 10';
+		}
+	
+		else {
+		include_once plugin_dir_path( __FILE__ ).'prices/price-add.php';
+		$npages = $_POST['npages'];
+		$pcase = $_POST['pcase'];
+		$np = new price_add($npages, $pcase);
+		$response['price'] = $np->get_price();
+		}
+
+    wp_send_json(json_encode($response));
+}
+
+?>
