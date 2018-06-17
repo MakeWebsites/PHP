ef042e76790b1bbb9ea63b4654c3fcd26f188554
Make-Websites <we@make-websites.co.uk>
2018-06-17 11:25:44 +0100

    Removing bootstrap

--- WordPress/Plugins/cc-by-country/cc-by-country.php
+++ WordPress/Plugins/cc-by-country/cc-by-country.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: cc-by-country
 Plugin URI: 
@@ -9,18 +8,15 @@ Author: ClimaRisk
 Author URI: www.climarisk.com
 License: GPLv2
 */
-
 function cc_by_c_scripts() {
 wp_register_script( 'gct', plugin_dir_url( __FILE__ ) .  '/includes/js/gct.js');
 }
 add_action('template_redirect', 'cc_by_c_scripts');
-
 // Loading the translation files
 add_action( 'plugins_loaded', 'cc_by_country_load_textdomain' );
 function cc_by_country_load_textdomain() {
   load_plugin_textdomain( 'cc-by-country', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
-
 // GCT function
         add_action( 'wp_ajax_gt', 'gt' );//admin
         add_action('wp_ajax_nopriv_gt', 'gt');//frontend
@@ -47,12 +43,11 @@ function cc_bc_gc ($atts) {
         $contents = ob_get_clean();
     return $contents;
     }
-
 $precv = array ('prec', 'Prec', 'pr', 'Precipitation');
  
 $atts = shortcode_atts( // Defaults include trends and links to ClimaRisk post 
         ['ctitle' => 'true', // Including a title - Default true
-	 'cclink' => 'true', // Including a link to Climaisk post - Default true
+	 'cclink' => 'false', // Including a link to Climaisk post - Default false
          'ctemp' => 'true', // Including the Temperature chart - Default true
          'cprec' => 'true', // Including the Precipitation chart - Default true
                         ], $atts, 'cc-by-country' );
@@ -71,34 +66,32 @@ if (filter_input(INPUT_POST, 'country2')) $c2 = $_POST['country2'];  else  {
     include_once "includes/country_ip.php";
     $c2d = new country_ip();
     $c2 = $c2d->getC2(); }
-
 // Estimating c3
     include_once 'includes/convC3.php';
     $c3d = new convC3($c2);
     $c3 = $c3d->getC3($c2);
                 
-
-
 include_once "includes/cctitle.php";
-  $sci = '<div class="row"><div class="col-md-8">';
-  $sci .= new cctitle($c2, $c3);
-  $sci .= '</div><div class="col-md-4">';
-    if ($cclink != 'false') { //Not set false the link
-	$clink = 'http://climarisk.com/'. __("en/climate-change-effects-country/", "cc-by-country");
+  $sci = '<a name="ccg"></a>';
+  if ($cclink === 'true') { //Including the link
+	 $clink = 'http://climarisk.com/'. __("en/climate-change-effects-country/", "cc-by-country");
         $infop = plugin_dir_url( __FILE__ ).'img/icon_info.gif';
-	$sci .= '<a href="'.$clink.'" target="_blank">
-	<img class="align-top" src="'.$infop.'">'.__("More information", "cc-by-country").'</a>';
-	}
-	$sci .= '</div></div>';
+	$sci .= '<div align="right"><a href="'.$clink.'" target="_blank">
+	<img class="align-top" src="'.$infop.'">'.__("Read more", "cc-by-country").'</a></div>';
+	 }
+  $sci .= new cctitle($c2, $c3);
+  
                 
     // Enqueue the registered scripts and draw
         wp_enqueue_script('google-jsapi','https://www.google.com/jsapi'); 
         wp_enqueue_script( 'gct' );   
     // Form    
-        $scf = '<form id="fms" class="form-inline" method="POST" action="'.get_permalink().'#ccg"><div class="form-group">';
-        $scf .= '<select class="custom-select" id="country2" name="country2"><option selected value="US">'.__('Select another country....', 'cc-by-country').'</option>';
+        $scf = '<form method="POST" action="'.get_permalink().'#ccg">';
+		$scf .= '<button type="submit" style="float:right">'.__("Submit", "cc-by-country").'</button>';
+		$scf .= '<div style="overflow: hidden; padding-right: .5em;">';
+        $scf .= '<select id="country2" name="country2"><option selected value="US">'.__('Select another country....', 'cc-by-country').'</option>';
         $scf .= get_f_content('includes/country_select.html');
-        $scf .= '</div><button type="submit" class="btn btn-primary ml-1">'.__("Submit", "cc-by-country").'</button>';
+        
         $scf .= '</form>';
         
     //Drawing charts
@@ -119,5 +112,4 @@ include_once "includes/cctitle.php";
 return $sci.$gc.$sf;	
 	
 }  
-
 add_shortcode('cc_by_country', 'cc_bc_gc')	;
